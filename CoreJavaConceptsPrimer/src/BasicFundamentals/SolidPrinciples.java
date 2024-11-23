743d6869b4f0f8ba79ef995cb1e626c1a60be27d
singh-abhinash <abhinashsingh2021po@gmail.com>
2024-11-24 05:28:50 +0530

    Added solid principles

---
+++ CoreJavaConceptsPrimer/src/BasicFundamentals/SolidPrinciples.java
@@ -0,0 +1,28 @@
+package BasicFundamentals;
+
+public class SolidPrinciples {
+
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/**
+ * The SOLID principles are five design principles for creating well-structured and maintainable object-oriented code. Here's a simple explanation:
+
+S - Single Responsibility Principle (SRP)
+A class should have only one reason to change, meaning it should only do one thing or have one responsibility.
+
+O - Open/Closed Principle (OCP)
+Code should be open for extension but closed for modification. You should be able to add new functionality without changing existing code.
+
+L - Liskov Substitution Principle (LSP)
+Objects of a parent class should be replaceable with objects of a child class without affecting the correctness of the program.
+
+I - Interface Segregation Principle (ISP)
+A class should not be forced to implement interfaces it doesn’t use. Break down large interfaces into smaller ones.
+
+D - Dependency Inversion Principle (DIP)
+High-level modules should not depend on low-level modules. Both should depend on abstractions (e.g., interfaces).
+
+Extra Topic:
+-> The full form of HATEOAS is Hypermedia As The Engine Of Application State.
+ */
